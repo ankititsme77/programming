475a039175f279a1522ee236f4cfefcb8dc6196c
Ankit Chaudhary <ankittisme77@gmail.com>
2023-05-17 10:50:55 -0700

    Add code sample for tree maximum depth

---
+++ CodeSamples/Tree/MaximumDepthOfTree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+
+        return findMaxDepth(root);
+
+    }
+
+    private int findMaxDepth(TreeNode root){
+
+        if(root == null)
+            return 0;
+
+        int left = findMaxDepth(root.left);
+
+        int right = findMaxDepth(root.right);
+
+        return Math.max(left,right) +1 ;
+
+
+
+
+    }
+}
