0871d66bde395788c275257e1a86086245f1c9f3
Ankit Chaudhary <ankittisme77@gmail.com>
2023-05-20 13:06:32 -0700

    Binary Tree- In order traversal

---
+++ CodeSamples/Tree/InOrderTraversal.java
@@ -0,0 +1,42 @@
+* Definition for a binary tree node.
+        * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> res = new ArrayList<>();
+
+        inOrderTreeTraversal(res,root);
+
+        return res;
+
+    }
+
+    private void inOrderTreeTraversal(List<Integer> res, TreeNode root){
+
+        if (root == null){
+            //res.add(null);
+            return;
+        }
+
+
+        inOrderTreeTraversal(res,root.left);
+
+        res.add(root.val);
+
+        inOrderTreeTraversal(res,root.right);
+
+    }
+
+}
